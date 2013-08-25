012d703694c6c4145f10f89bfa0e8d5403a2c0ab
wangjizhe <buptjz@gmail.com>
2013-08-25 15:33:55 +0800

    12

--- CiCT/CiCT/main.c
+++ CiCT/CiCT/main.c
@@ -8,11 +8,27 @@
 
 #include <stdio.h>
 
+int isUnique(char a[]){
+    int i = 0;
+    while (a[i]!='\0') {
+        for (int j = 0; j<i; j++) {
+            if (a[j] == a[i]) {
+                return 0;
+            }
+        }
+        i++;
+    }
+    return 1;
+}
+
 int main(int argc, const char * argv[])
 {
 
     // insert code here...
     printf("Hello, World!\n");
+    char a[] = "abcdefge";
+    int is = isUnique(a);
+    printf("%d",is);
     return 0;
 }
 
