9c03f842cb26747a58e9d9569babd25231b37c0f
wangjizhe <buptjz@gmail.com>
2013-08-25 15:25:16 +0800

    InitCiCT

---
+++ CiCT/CiCT/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  CiCT
+//
+//  Created by Wang JZ on 13-8-25.
+//  Copyright (c) 2013年 Wang JZ. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
