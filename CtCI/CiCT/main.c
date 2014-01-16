03b02de6069e440aa9dda01d935f79cb85517664
buptjz <buptjz@gmail.com>
2014-01-16 14:22:53 +0800

    改正了错误：在旋转数组中搜索

--- CtCI/CiCT/main.c
+++ CtCI/CiCT/main.c
@@ -12,6 +12,6 @@
 
 int main(int argc, const char * argv[])
 {
-    testTrackStream();
+    testSearchInRotateArray();
     return 0;
 }
--- CtCI/CiCT/searchInRotatedArray.c
+++ CtCI/CiCT/searchInRotatedArray.c
@@ -17,32 +17,40 @@ int searchInRotateArray(int *A,int start,int end,int target){
     
     int mid = start + ((end - start) >> 1);
     if (A[mid] == target) return mid; //找到，立即返回
-    else if (A[mid] == A[start] && A[mid] == A[end]) {
+    
+    if (A[mid] == A[start] && A[mid] == A[end]) {
         int result = searchInRotateArray(A, start, mid-1,target);
-        if (result == -1) {//先在左边找，如果没找到，再去右边找，不能用||，因为这里return的是-1而不是0
+        if (result == -1) {
+            //先在左边找，如果没找到，再去右边找，不能用||，因为这里return的是-1而不是0
             result = searchInRotateArray(A, mid+1, end,target);
         }
         return result;
-    }
-    else if (A[mid] > target) {//处理target小的情况
-        if (A[end] >= A[mid]) {//右串是排序的，（或者是重复），所以都大于target，所以不用搜索右串
+    }else if(A[start] < A[mid]) {//左串排好序的
+        if (target < A[mid] &&  target >= A[start]) {
+            //x夹在左、中之间，那么右串都是在A[start]~A[mid]范围之外的，所以不用考虑
             return searchInRotateArray(A, start, mid-1,target);
         }else{
             return searchInRotateArray(A, mid+1, end,target);
         }
-    }else{//处理target大的情况
-        if (A[start] <= A[mid]) {//左串是排序的（或者是重复的），所以都小于target，所以不用搜索左串
+    }else if(A[start] > A[mid]){//右串排序了,很关键
+        if (target <= A[end] &&  target > A[mid]) {
+            //x夹在右、中之间，那么左串都是>A[end],<A[mid]的，所以不用考虑
             return searchInRotateArray(A, mid+1, end,target);
         }else{
             return searchInRotateArray(A, start, mid-1,target);
         }
+    }else if(A[start] == A[mid]){
+        //特殊条件：左串是重复的，而右串非重复，也就是A[start]和A[mid]中间所有的数都是重复的，那么只在右边找
+        return searchInRotateArray(A, mid+1, end,target);
     }
+    
+    return -1;
 }
 
 void testSearchInRotateArray(){
     int a[5] = {3,4,5,1,2};
-    int r1 = searchInRotateArray(a, 0, 4, 1);
-    printf("Expected %d and get %d\n",3,r1);
+    int r1 = searchInRotateArray(a, 0, 4, 3);
+    printf("Expected %d and get %d\n",1,r1);
     
     int b[10] = {1,1,1,1,1,1,0,1,1,1};
     int r2 = searchInRotateArray(b, 0, 9, 0);
