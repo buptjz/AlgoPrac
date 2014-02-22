7046425c398805538fd95772005aca8a6cbddfee
buptjz <buptjz@gmail.com>
2014-02-22 23:51:48 +0800

    完成链表交换俩数的操作

---
+++ LeetCode/LeetCode/SwapNodesinPairs.cpp
@@ -0,0 +1,45 @@
+//
+//  SwapNodesinPairs.cpp
+//  LeetCode
+//
+//  Created by WangJZ on 14-2-22.
+//  Copyright (c) 2014年 WangJZ. All rights reserved.
+//  http://oj.leetcode.com/problems/swap-nodes-in-pairs/
+
+#include "SwapNodesinPairs.h"
+
+struct ListNode{
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+
+ListNode* swapPairs(ListNode *head)
+{
+    if (head == NULL) {
+        return head;
+    }
+    
+    ListNode *f,*s;
+    f = head;
+    s = f->next;
+    if(s == NULL){
+        return head;
+    }
+    f->next = s->next;
+    s->next = f;
+    head = s;
+    while (f->next && f->next->next) {//下两个节点都存在
+        f = f->next;
+        s = s->next;
+        s->next = f->next;
+        s = f->next;
+        f->next = s->next;
+        s->next = f;
+    }
+    return head;
+}
+void testSwapPairs(){
+    
+}
