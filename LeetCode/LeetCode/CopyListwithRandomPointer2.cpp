9ee966fcd50afe221e4b51573707b4979bd1e7ba
buptjz <buptjz@gmail.com>
2014-03-13 15:35:08 +0800

    添加了一点注释

--- LeetCode/LeetCode/CopyListwithRandomPointer2.cpp
+++ LeetCode/LeetCode/CopyListwithRandomPointer2.cpp
@@ -4,7 +4,14 @@
 //
 //  Created by WangJZ on 14-3-13.
 //  Copyright (c) 2014年 WangJZ. All rights reserved.
-//
+//  http://oj.leetcode.com/problems/copy-list-with-random-pointer/
+
+/*
+ 上一个思路用时是O(N2)
+ 这个代码使用了HashMap，在第一轮遍历的时候将原始节点与copy的节点一一对应
+ 在第二轮遍历的时候在HashMap中就可以以O(1)的时间复杂度读取任意一个节点，这样就能保证整体的时间复杂度
+ 
+ */
 
 #include "CopyListwithRandomPointer2.h"
 #include <map>
