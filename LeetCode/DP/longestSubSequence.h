2b5e735be1e3017f6d891f82255d042b8dd142e4
buptjz <buptjz@gmail.com>
2014-03-15 23:19:47 +0800

    改变了文件的结构

---
+++ LeetCode/DP/longestSubSequence.h
@@ -0,0 +1,14 @@
+//
+//  longestSubSequence.h
+//  LeetCode
+//
+//  Created by WangJZ on 14-3-15.
+//  Copyright (c) 2014年 WangJZ. All rights reserved.
+//
+
+#ifndef __LeetCode__longestSubSequence__
+#define __LeetCode__longestSubSequence__
+
+#include <iostream>
+
+#endif /* defined(__LeetCode__longestSubSequence__) */
