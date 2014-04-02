27b193de7e0939fbe709faf4a49121e697369391
buptjz <buptjz@gmail.com>
2014-04-02 15:14:36 +0800

    完成Populating Next Right Pointers in Each Node

---
+++ LeetCode/Tree/PopulatingNextRightPointersinEachNode.cpp
@@ -0,0 +1,39 @@
+//
+//  PopulatingNextRightPointersinEachNode.cpp
+//  Algorithms
+//
+//  Created by WangJZ on 14-4-2.
+//  Copyright (c) 2014年 WangJZ. All rights reserved.
+//  http://oj.leetcode.com/problems/populating-next-right-pointers-in-each-node/
+
+//又见树的遍历，请画图仔细思考
+
+#include "leetcode_tree.h"
+
+struct TreeLinkNode {
+    int val;
+    TreeLinkNode *left, *right, *next;
+    TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+};
+
+void firstTraverse(TreeLinkNode *root){
+    if (root == NULL) return;
+    if (root->left) root->left->next = root->right;
+    firstTraverse(root->left);
+    firstTraverse(root->right);
+}
+void secondTraverse(TreeLinkNode *root){
+    if (root == NULL) return;
+    if (root->right && root->next) root->right->next = root->next->left;
+    secondTraverse(root->left);
+    secondTraverse(root->right);
+}
+void connect(TreeLinkNode *root) {
+    //分两边递归，第一遍令左孩子的next指向兄弟t
+    //第二遍改变所有右孩子的next，如果父节点的next是空的，那么,右孩子的next为空，如果不为空，那么
+    //rightchild->next = parent->next->left;
+    firstTraverse(root);
+    secondTraverse(root);
+}
+
+
