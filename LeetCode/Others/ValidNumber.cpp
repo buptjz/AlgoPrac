173f9289c034972e83b154e06715fd709ac09a50
buptjz <buptjz@gmail.com>
2014-04-04 09:53:06 +0800

    借鉴了别人的Valid Number

---
+++ LeetCode/Others/ValidNumber.cpp
@@ -0,0 +1,61 @@
+//
+//  ValidNumber.cpp
+//  Algorithms
+//
+//  Created by WangJZ on 14-4-4.
+//  Copyright (c) 2014年 WangJZ. All rights reserved.
+//
+
+#include "leetcode_others.h"
+bool isNumber(const char *s) {
+    if (s == NULL || s[0] == '\0') return false;
+    bool cansign = true;
+    bool cane = false;
+    bool havee = false;
+    bool candot = true;
+    bool onlyspace = false;
+    bool havenum = false;
+    bool numbegin = false;
+    while(*s != '\0') {
+        char c = *(s++);
+        if (c == ' '){
+            if (numbegin)
+                onlyspace = true;
+            continue;//skip space
+        } else if (onlyspace) {
+            return false;
+        }
+        if (c == '+' || c == '-') {
+            if(!cansign) return false;
+            cansign = false;
+            numbegin = true;
+            continue;
+        }
+        if (c == 'e') {
+            if(!cane) return false;
+            cane = false;
+            havenum = false;
+            numbegin = true;
+            cansign = true;
+            havee = true;
+            candot = false;
+            continue;
+        }
+        if (c == '.') {
+            if(!candot) return false;
+            candot = false;
+            numbegin = true;
+            cansign = false;
+            continue;
+        }
+        if (c >= '0' && c <= '9') {
+            havenum = true;
+            numbegin = true;
+            cansign = false;
+            if(!havee) cane = true;
+        } else {
+            return false;
+        }
+    }
+    return havenum;
+}
