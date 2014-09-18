05e69eb2c3ee0159582fd87981e9320a5d491f3a
buptjz <buptjz@gmail.com>
2014-09-18 15:36:18 +0800

    写了 Single Number

---
+++ LeetCodeReview/LeetCodeReview/bitManipulate/SingleNumber.cpp
@@ -0,0 +1,18 @@
+//
+//  SingleNumber.cpp
+//  LeetCodeReview
+//
+//  Created by WangJZ on 14-9-18.
+//  Copyright (c) 2014年 WangJZ. All rights reserved.
+//
+
+#include "leetcode_bit.h"
+
+
+int singleNumber(int A[], int n) {
+    int retVal = 0;
+    for (int i = 0; i < n; i++) {
+        retVal ^= A[i];
+    }
+    return retVal;
+}
