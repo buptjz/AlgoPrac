dd15b0762657358e05a70b6ddd0ad7ee877c28b4
buptjz <buptjz@gmail.com>
2014-09-22 16:01:39 +0800

    完成 判断一棵树是否是对称的

---
+++ LeetCodeReview/LeetCodeReview/tree/SymmetricTree.cpp
@@ -0,0 +1,21 @@
+//
+//  SymmetricTree.cpp
+//  LeetCodeReview
+//
+//  Created by WangJZ on 14-9-22.
+//  Copyright (c) 2014年 WangJZ. All rights reserved.
+//
+
+#include "leetcode_tree.h"
+
+static bool helper(TreeNode *t1,TreeNode *t2){
+    if (t1 == NULL && t2 == NULL) return true;
+    if (t1 == NULL || t2 == NULL) return false;
+    if (t1->val != t2->val ) return false;
+    return helper(t1->left, t2->right) && helper(t1->right, t2->left);
+}
+
+bool isSymmetric(TreeNode *root) {
+    if (root == NULL) return true;
+    return helper(root->left, root->right);
+}
